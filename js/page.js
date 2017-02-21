e17cd87b6c0534997d1c4f1a7ceb647cded49b2f
Ryan Boone <falldowngoboone@gmail.com>
2017-02-20 20:53:21 -0600

    Toggle header

--- js/page.js
+++ js/page.js
@@ -1,8 +1,24 @@
 onload = function () {
+  var pageHeader = document.querySelector('.js-page-header'),
+      menuToggle = document.querySelector('.js-menu-toggle')
+
   // js class for styles relying on js functionality
   document.documentElement.classList.add('js')
 
   // make sure copyright is always up-to-date
   document.querySelector('#copyright-date')
     .innerText = (new Date()).getFullYear()
+
+  menuToggle.addEventListener('click', toggleMenuOpen)
+  pageHeader.addEventListener('click', function (e) {
+    if (e.target.nodeName === 'A' ) {
+      toggleMenuOpen()
+    }
+  })
+
+  function toggleMenuOpen () {
+    pageHeader.classList.contains('is-collapsed') ?
+      pageHeader.classList.remove('is-collapsed') :
+      pageHeader.classList.add('is-collapsed')
+  }
 }
